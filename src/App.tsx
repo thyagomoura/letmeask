f14ca024e60003a007f0cb917152dd648f6c8d09
thyago <thyagohenriquemoura@gmail.com>
2021-06-22 19:15:02 -0300

    Alteraçoes da segunda aula, com hooks, autenticaçoes, rotas, configuraçoes do firebase, contextos, styles e sign in com google

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import {Button} from './components/Button'
+import {Home} from './pages/Home'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { NewRoom } from './pages/NewRoom';
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
+
   return (
-    <div>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+          <Route path="/" exact component={Home}/>
+          <Route path="/rooms/new" component={NewRoom}/>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
